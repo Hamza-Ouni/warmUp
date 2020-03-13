6188915d4ca60aa3f558bd0baa305a05031a56b7
Hamza-Ouni <Hamza-ouni@outlook.com>
2020-03-13 10:55:02 +0100

    solved

--- warmUp1.js
+++ warmUp1.js
@@ -4,3 +4,45 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+function partnerName(firstName,lastName){
+        return firstName +' ' + lastName;
+
+}
+
+
+function multiple(n){
+     if (n % 13 === 0) {
+     	return ('n is a multiple of 13');
+     }
+     else 
+     	return ('n is not a multiple of 13');
+}
+
+
+function ageInSecond (x){
+	return ( x*365*24*60*60 );
+}
+
+
+function averageAge(age){
+    var sum=0;
+        for (var i=0;i<age.length;i++){
+           sum=sum+age[i]
+           average = sum/age.length     
+}
+return average;
+        }
+
+
+function averageAge(age){
+    var sum=0;
+        for (var i=0;i<age.length;i++){
+           sum=sum+age[i]
+           average = sum/age.length     
+}
+return average;
+        }
+var age =[13,14,13,15,16,17,19,13,16,15];
+
