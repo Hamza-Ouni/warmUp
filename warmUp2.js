d02b40d6336eabf4e91a8adcdbde27ad9d061843
Hamza-Ouni <Hamza-ouni@outlook.com>
2020-03-14 12:04:57 +0100

    practice

--- warmUp2.js
+++ warmUp2.js
@@ -1,19 +1,47 @@
 // 1 - Complete the function cube that returns the cube of x:
 
 function cube(x) {
-  // your code here
+return x*x*x;
 }
 
 // 2 - Write a function sameLength that accepts two strings as arguments, and returns true if those strings have the same length, and false otherwise.
 
 function sameLength(string1, string2) {
-  // your code here
+	if (string1.length === string2.length){
+		return true;
+
+	}
+else return false;
 }
+
 
 // 3 - Write a function called scoreToGrade that accepts a number as a parameter and returns a string representing a letter grade corresponding to that score.
 
 function scoreToGrade(number) {
-  // your code here
+
+  if (score > 90 && score <= 100){
+    return 'A'
+  } 
+
+  else if (score >= 80 && score < 90) {
+    return 'B'
+  } 
+
+  else if (score >= 85 && score < 80) {
+    return 'C'
+  } 
+
+  else if (score >= 50 && score < 65) {
+    return 'D' 
+} 
+    
+    else if (score <= 50) {
+    return 'E' 
+} 
+    else{
+    return 'stupid'
+  }
+}
 }
 // USE RECURSION
 // Repeating a String n Times: Let's write a function called repeatString that takes two parameters: a string str, which is the string to be repeated, and count -- a number representing how many times the string str should be repeated
@@ -23,5 +51,10 @@ function scoreToGrade(number) {
 // repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
 
 function repeatString(str, count) {
-  // TODO: your code here
+if (count === 0){
+  return '';
+  {
+  	return str + repeatString(str, count-1); 
+  }
+
 }
