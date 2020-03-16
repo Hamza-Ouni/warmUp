a0501d3a17c39fae81bbc668005d182192e45062
hamza-ouni <hamza-ouni@outlook.com>
2020-03-16 10:04:21 +0100

    practice 3

--- warmUp3.js
+++ warmUp3.js
@@ -1,8 +1,26 @@
 // 1 - Write functions larger and smaller that each accept two strings as arguments, and return the larger and smaller strings, respectively
+function laregerAndSmaller (str1, str2){
+	if (str1.length > str2.length) {
+		return str1 +','+ str2
+	}
+	else 
+		return str2 +','+ str1
+}
+
 
 // 2 - Write a function called counter that, when invoked, always returns a number that is one more than the previous invocation. For instance:
-// function counting(n) { 
-    TODO: your code here 
+// 
+function counting(n) {
+	var str='';
+	var result=0;
+		for (var a=1 ; a<n ; a++) {
+			str = str +a +',';
+  		 	}
+			result = str +n;
+
+		return result ;
+			}
+
 // } 
 // counting(5); // => '1, 2, 3, 4, 5' 
 // counting(1); // => '1' 
@@ -15,7 +33,22 @@
 //  welcome 3, meet 1 and 2
 //  welcome 4, meet 1, 2 and 3
 //  welcome 5, meet 1, 2, 3 and 4
+function meetAndGreet (n){    
+		for (var i=1 ; n>0 ; i++)		
+			if (n=1){
+				return 'welcome'+n;
+			}
+			else if (n=2){
+				return 'welcome'+n+' '+ 'meet'+i;
+			}
+			else if (n=3){
+				return 
+			}
+		
 //
 
 // 4- Write a function shortestWord that works like longestWord, but returns the shortest word instead.
+function shortestWord (string){
+
+}
 
