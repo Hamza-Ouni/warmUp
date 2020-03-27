7d9f99e8dde4b3170510870ddcc5e371b78ca6d1
hamza-ouni <hamza-ouni@outlook.com>
2020-03-27 09:39:18 +0100

    warmUp done

--- warmUp13.js
+++ warmUp13.js
@@ -5,8 +5,23 @@
 //starting from 0, e.g.:
 // mult(3); // => 6
 // mult(4); // => 24
-
+function mult(n){
+	if (n === 1){
+		return 1;
+	}
+	return n * mult(n-1);
+}
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number.
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+function setOfNumber(n) {
+   var init = "1";
+   var i =2;
+   var str = "";
+	while (i <= n){
+   		str = str + i + " " + i + " " ;
+        i++;
+	}
+	return init + " " + str;
+}
